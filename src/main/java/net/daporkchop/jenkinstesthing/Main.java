8478418921c725fd2cad29421c4af5b263c88665
DaPorkchop_ <daporkchop@daporkchop.net>
2020-04-07 10:06:33 +0200

    add main class

---
+++ src/main/java/net/daporkchop/jenkinstesthing/Main.java
@@ -0,0 +1,10 @@
+package net.daporkchop.jenkinstesthing;
+
+/**
+ * @author DaPorkchop_
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Hello World!");
+    }
+}
